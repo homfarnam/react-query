6faf219c3728743753c5fea030f9828565091fd8
homfarnam <homfarnam@gmail.com>
2020-07-30 21:50:06 +0430

    use usePaginationQuery

--- src/components/planet/Planets.js
+++ src/components/planet/Planets.js
@@ -1,41 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { useQuery } from "react-query";
+import React, { useState } from "react";
+import { usePaginatedQuery } from "react-query";
 import Planet from "./Planet";
 
-const fetchPlanets = async (key, greeting, page) => {
-  console.log(greeting);
+const fetchPlanets = async (key, page) => {
   const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
   const [page, setpage] = useState(1);
-  const { data, status } = useQuery(
-    ["planets", "hello world", page],
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ["planets", page],
     fetchPlanets
   );
-  console.log(data);
 
   return (
     <div>
       <h2>Planets</h2>
-      <div className='buttons'>
-        <button onClick={() => setpage(page - 1)}>Before Page</button>
-
-        <p>Page {page}</p>
-        {/* <button onClick={()=> setpage(2)}>Page 2</button>
-<button onClick={()=> setpage(3)}>Page 3</button> */}
-        <button onClick={() => setpage(page + 1)}>Next Page</button>
-      </div>
+     
 
       {status === "loading" && <div>Loading data</div>}
       {status === "error" && <div>Error fetching data</div>}
+
       {status === "success" && (
-        <div>
-          {data.results.map((planet) => (
-            <Planet planet={planet} key={planet.name} />
-          ))}
-        </div>
+        <>
+          <button
+          onClick={()=> setpage(old=> Math.max(old-1,1))}
+          disabled={page===1}
+          >Previous Page</button>
+          <span>{page}</span>
+          <button
+          onClick={()=> setpage(old=>(!latestData || !latestData.next ? old : old+1))}
+          disabled={!latestData || !latestData.next}
+          >Next Page</button>
+
+          <div>
+            {resolvedData.results.map((planet) => (
+              <Planet planet={planet} key={planet.name} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
